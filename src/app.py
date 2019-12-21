5b11b7986543e29eb7e0b37efe7917324fa6c272
Ravi Shanker Reddy Karri <me@ravikarri.in>
2019-12-22 01:27:44 +0530

    use sam build --use-container

--- src/app.py
+++ src/app.py
@@ -1,13 +1,41 @@
 import json
-from .response import handler
 # import requests
 
 
 def lambda_handler(event, context):
+    """Sample pure Lambda function
 
-    result = {
-            "message": event['pathParameters']['Name'],
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
+
+    #     raise e
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "DpneS",
             # "location": ip.text.replace("\n", "")
-        }
-    
-    handler(200,result)
+        }),
+    }

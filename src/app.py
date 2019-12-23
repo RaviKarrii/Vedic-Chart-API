7216abaff693e4486ecbfbcc64f79b2017d5113b
Ravi Shanker Reddy Karri <me@ravikarri.in>
2019-12-24 00:27:38 +0530

    Adding functionality

--- src/app.py
+++ src/app.py
@@ -4,38 +4,14 @@ import codebase as cb
 
 
 def lambda_handler(event, context):
-    """
-    Body Will be 
 
-    [
-  {
-  "Male":
-  {
-    Year: '1993',
-    Month: '09',
-    Day: '16',
-    Hour: '17',
-    Min: '55',
-    Lat:'16.989',
-    Lon: '82.247'
+    input_payload = json.loads(event['body'])
     
-  }
-}
-]
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": str(cb.convert_angle(190)),
-            # "location": ip.text.replace("\n", "")
+        "body": json.dumps({            
+        "message": input_payload['Year'],
+        # "location": ip.text.replace("\n", "")
         }),
     }
